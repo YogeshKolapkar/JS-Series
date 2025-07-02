8a05ca6483eb86918a0b42866437b3fdc954c881
Yogesh Kolapkar <130735596+YogeshKolapkar@users.noreply.github.com>
2025-07-02 06:38:26 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,28 @@
+// Datatypes in JS
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // We are using Node.js Not Brower.
+
+let name ="Yogesh" //string Datatype
+let age = 25 //Number Datatypes
+let isLoggedIn = true // boolean Datatypes
+
+
+/* DataTypes in Js
+Primitive Datatypes:
+    number => 2 to power 53
+    bigInt
+    String => use "" or '' for String but prefer always Double Quotes i.e .""
+    boolean => true or false
+    null => stand alone value
+    undefined => 
+    symbol=> use for uniqueness
+
+Non-primitive Datatypes :
+    Object 
+        
+
+
+
+*/
+console.log(typeof age) // typeof is use to check the datatypes.
