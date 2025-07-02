3fbf94383b21d5f7005bfe2ae0d886f424347401
Yogesh Kolapkar <130735596+YogeshKolapkar@users.noreply.github.com>
2025-07-02 10:06:40 +0000

    Comparision

---
+++ 01_Basics/04_comparisionOfDatatype.js
@@ -0,0 +1,21 @@
+// ComparisionOF Datatypes
+/* comparision and Equality check Work Differently*/
+
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log(2==1)
+
+
+// console.log("2">1)
+// console.log(null>0)
+// console.log(null>=0)
+
+
+// console.log(undefined >0);
+// console.log(undefined==0);
+
+// console.log(undefined>=0);
+
+// strict check "===" => 
+console.log(2===2);
