769a25f1637d4771edc48f6561ddb7fb98af5900
Yogesh Kolapkar <130735596+YogeshKolapkar@users.noreply.github.com>
2025-07-02 07:01:23 +0000

    Conversion Operation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+// Conversion Operations:
+let score ="33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score) // Number() is Use to convert into number datatypes.
+console.log(typeof valueInNumber)
+
+//"33 " =>33
+// "33sbc" => Nan =>Not a Number
+// true =>1 or false =>0
+
+let isLoggedIn = "Yogesh";
+let booleanIsLoggedIn = Boolean(isLoggedIn) //Boolean() is to convert into boolean value
+// console.log(booleanIsLoggedIn);
+
+//1=>true or  0=>false
+// " "=> false
+// "Yogesh" => true 
+
+let someNumber =66
+let stringNumber = String(someNumber) //String() is use to convert into String
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
