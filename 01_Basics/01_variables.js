c7d0e0ac1ecff7ed5ec8c713f1720cebce5a53f6
Yogesh Kolapkar <130735596+YogeshKolapkar@users.noreply.github.com>
2025-07-02 06:16:32 +0000

    Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 16546 // const neither be updated nor re-declared  variables created with "const" cannot change.
+let accountEmail ="kolapkaryogesh***@gmail.com"// variables created with "let "you can update but not re-declared.
+var accountPassword = "12354"
+accountCity = "Nannaj"
+let accountState ;
+
+console.log()
+
+accountEmail="yogeshkolapkar***@gmail.com"
+accountPassword ="7984164";
+accountCity ="Pune"
+
+console.table([accountId,accountEmail,accountPassword, accountCity, accountState])
